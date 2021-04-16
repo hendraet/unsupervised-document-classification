86190c71fbb7eaa55c8a7456760d96e37c3acfeb
Jona Otholt <18723283+J-L-O@users.noreply.github.com>
2021-04-16 12:26:06 +0200

    Allow for multiple pretrained moco options

--- moco.py
+++ moco.py
@@ -8,8 +8,8 @@ import torch
 import numpy as np
 
 from utils.config import create_config
-from utils.common_config import get_model, get_train_dataset,\
-                                get_val_dataset, get_val_dataloader, get_val_transformations
+from utils.common_config import get_model, get_train_dataset, \
+    get_val_dataset, get_val_dataloader, get_val_transformations
 from utils.memory import MemoryBank
 from utils.utils import fill_memory_bank
 from termcolor import colored
@@ -23,12 +23,12 @@ parser.add_argument('--config_exp',
 parser.add_argument('--tb_run', help='Tensorboard log directory')
 args = parser.parse_args()
 
+
 def main():
     # Retrieve config file
     p = create_config(args.config_env, args.config_exp, args.tb_run)
     print(colored(p, 'red'))
-    
-    
+
     # Model
     print(colored('Retrieve model', 'blue'))
     model = get_model(p)
@@ -36,23 +36,20 @@ def main():
     print(model)
     model = torch.nn.DataParallel(model)
     model = model.cuda()
-   
-    
+
     # CUDNN
-    print(colored('Set CuDNN benchmark', 'blue')) 
+    print(colored('Set CuDNN benchmark', 'blue'))
     torch.backends.cudnn.benchmark = True
-    
-    
+
     # Dataset
     print(colored('Retrieve dataset', 'blue'))
     transforms = get_val_transformations(p)
-    train_dataset = get_train_dataset(p, transforms) 
+    train_dataset = get_train_dataset(p, transforms)
     val_dataset = get_val_dataset(p, transforms)
     train_dataloader = get_val_dataloader(p, train_dataset)
     val_dataloader = get_val_dataloader(p, val_dataset)
     print('Dataset contains {}/{} train/val samples'.format(len(train_dataset), len(val_dataset)))
-    
-   
+
     # Memory Bank
     print(colored('Build MemoryBank', 'blue'))
     memory_bank_train = MemoryBank(len(train_dataset), 2048, p['num_classes'], p['temperature'])
@@ -60,13 +57,10 @@ def main():
     memory_bank_val = MemoryBank(len(val_dataset), 2048, p['num_classes'], p['temperature'])
     memory_bank_val.cuda()
 
-    
     # Load the official MoCoV2 checkpoint
     print(colored('Downloading moco v2 checkpoint', 'blue'))
-    os.system('wget -L https://dl.fbaipublicfiles.com/moco/moco_checkpoints/moco_v2_800ep/moco_v2_800ep_pretrain.pth.tar')
-    moco_state = torch.load('moco_v2_800ep_pretrain.pth.tar', map_location='cpu')
+    moco_state = torch.load(p['pretrained'], map_location='cpu')
 
-    
     # Transfer moco weights
     print(colored('Transfer MoCo weights to model', 'blue'))
     new_state_dict = {}
@@ -76,45 +70,41 @@ def main():
         if k.startswith('module.encoder_q') and not k.startswith('module.encoder_q.fc'):
             new_k = 'module.backbone.' + k[len('module.encoder_q.'):]
             new_state_dict[new_k] = state_dict[k]
-        
+
         # Copy mlp weights
         elif k.startswith('module.encoder_q.fc'):
-            new_k = 'module.contrastive_head.' + k[len('module.encoder_q.fc.'):] 
-            new_state_dict[new_k] = state_dict[k] 
+            new_k = 'module.contrastive_head.' + k[len('module.encoder_q.fc.'):]
+            new_state_dict[new_k] = state_dict[k]
 
         else:
-            raise ValueError('Unexpected key {}'.format(k)) 
+            raise ValueError('Unexpected key {}'.format(k))
 
     model.load_state_dict(new_state_dict)
-    os.system('rm -rf moco_v2_800ep_pretrain.pth.tar')
-   
- 
+
     # Save final model
     print(colored('Save pretext model', 'blue'))
     torch.save(model.module.state_dict(), p['pretext_model'])
-    model.module.contrastive_head = torch.nn.Identity() # In this case, we mine the neighbors before the MLP. 
+    model.module.contrastive_head = torch.nn.Identity()  # In this case, we mine the neighbors before the MLP.
 
-    
     # Mine the topk nearest neighbors (Train)
     # These will be used for training with the SCAN-Loss.
     topk = 50
-    print(colored('Mine the nearest neighbors (Train)(Top-%d)' %(topk), 'blue'))
+    print(colored('Mine the nearest neighbors (Train)(Top-%d)' % (topk), 'blue'))
     transforms = get_val_transformations(p)
-    train_dataset = get_train_dataset(p, transforms) 
+    train_dataset = get_train_dataset(p, transforms)
     fill_memory_bank(train_dataloader, model, memory_bank_train)
     indices, acc = memory_bank_train.mine_nearest_neighbors(topk)
-    print('Accuracy of top-%d nearest neighbors on train set is %.2f' %(topk, 100*acc))
+    print('Accuracy of top-%d nearest neighbors on train set is %.2f' % (topk, 100 * acc))
     np.save(p['topk_neighbors_train_path'], indices)
-   
-     
+
     # Mine the topk nearest neighbors (Validation)
     # These will be used for validation.
     topk = 5
-    print(colored('Mine the nearest neighbors (Val)(Top-%d)' %(topk), 'blue'))
+    print(colored('Mine the nearest neighbors (Val)(Top-%d)' % (topk), 'blue'))
     fill_memory_bank(val_dataloader, model, memory_bank_val)
     print('Mine the neighbors')
     indices, acc = memory_bank_val.mine_nearest_neighbors(topk)
-    print('Accuracy of top-%d nearest neighbors on val set is %.2f' %(topk, 100*acc))
+    print('Accuracy of top-%d nearest neighbors on val set is %.2f' % (topk, 100 * acc))
     np.save(p['topk_neighbors_val_path'], indices)
 
 
