8d0ac6fa9e93fc547f11ee39d4d1a597e130d07c
J-L-O <18723283+J-L-O@users.noreply.github.com>
2021-06-07 22:43:06 +0200

    Update tutorial_nn.py

--- tutorial_nn.py
+++ tutorial_nn.py
@@ -4,6 +4,7 @@ Licensed under the CC BY-NC 4.0 license (https://creativecommons.org/licenses/by
 """
 import argparse
 import os
+import torch
 import numpy as np
 
 from utils.config import create_config
@@ -23,12 +24,13 @@ parser.add_argument('--config_env',
                     help='Config file for the environment')
 parser.add_argument('--config_exp',
                     help='Config file for the experiment')
+parser.add_argument('--tb_run', help='Tensorboard log directory')
 args = parser.parse_args()
 
 def main():
 
     # Retrieve config file
-    p = create_config(args.config_env, args.config_exp)
+    p = create_config(args.config_env, args.config_exp, args.tb_run)
     print(colored(p, 'red'))
     
     # Model
@@ -73,26 +75,27 @@ def main():
     # Save model
     torch.save(model.state_dict(), p['pretext_model'])
 
-    # Mine the topk nearest neighbors at the very end (Train) 
+    # Mine the topk nearest neighbors at the very end (Train)
     # These will be served as input to the SCAN loss.
     print(colored('Fill memory bank for mining the nearest neighbors (train) ...', 'blue'))
     fill_memory_bank(base_dataloader, model, memory_bank_base)
     topk = 20
-    print('Mine the nearest neighbors (Top-%d)' %(topk)) 
-    indices, acc = memory_bank_base.mine_nearest_neighbors(topk)
-    print('Accuracy of top-%d nearest neighbors on train set is %.2f' %(topk, 100*acc))
-    np.save(p['topk_neighbors_train_path'], indices)   
+    print('Mine the nearest neighbors (Top-%d)' %(topk))
+    knn_indices, knn_acc = memory_bank_base.mine_nearest_neighbors(topk)
+    print('Accuracy of top-%d nearest neighbors on train set is %.2f' % (topk, 100 * knn_acc))
+    np.save(p['topk_neighbors_train_path'], knn_indices)
 
-    # Mine the topk nearest neighbors at the very end (Val)
+    # Mine the topk nearest neighbors (Validation)
     # These will be used for validation.
-    print(colored('Fill memory bank for mining the nearest neighbors (val) ...', 'blue'))
-    fill_memory_bank(val_dataloader, model, memory_bank_val)
     topk = 5
-    print('Mine the nearest neighbors (Top-%d)' %(topk)) 
-    indices, acc = memory_bank_val.mine_nearest_neighbors(topk)
-    print('Accuracy of top-%d nearest neighbors on val set is %.2f' %(topk, 100*acc))
-    np.save(p['topk_neighbors_val_path'], indices)   
+    print(colored('Mine the nearest neighbors (Val)(Top-%d)' % (topk), 'blue'))
+    fill_memory_bank(val_dataloader, model, memory_bank_val)
+    print('Mine the neighbors')
+    knn_indices, knn_acc = memory_bank_val.mine_nearest_neighbors(topk)
+    print('Accuracy of top-%d nearest neighbors on val set is %.2f' % (topk, 100 * knn_acc))
+    np.save(p['topk_neighbors_val_path'], knn_indices)
 
  
 if __name__ == '__main__':
     main()
+
